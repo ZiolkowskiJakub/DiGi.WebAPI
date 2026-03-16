ffc93a84b4cca4002007cab3df89f7891f06014c
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2026-03-16 22:18:52 +0100

    UrlBuilder implemented

---
+++ DiGi.WebAPI/Classes/UrlBuilder.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+namespace DiGi.WebAPI.Classes
+{
+    public class UrlBuilder
+    {
+        private readonly Dictionary<string, string> dictionary = [];
+        private readonly string url;
+        public UrlBuilder(string url)
+        {
+            this.url = url;
+        }
+
+        public IEnumerable<string> ParameterNames
+        {
+            get
+            {
+                return dictionary.Keys;
+            }
+        }
+
+        /// <summary>
+        /// Adds a query parameter with automatic URL encoding.
+        /// </summary>
+        public UrlBuilder AddParameter(string name, string? value)
+        {
+            if(name is null)
+            {
+                return this;
+            }
+
+            if (value != null)
+            {
+                // WebUtility.UrlEncode handles special characters like / # & ?
+                dictionary[name] = WebUtility.UrlEncode(value);
+            }
+
+            return this;
+        }
+
+        public UrlBuilder AddParameter(string name, int value)
+        {
+            return AddParameter(name, value.ToString());
+        }
+
+        public UrlBuilder AddParameter(string name, double value)
+        {
+            return AddParameter(name, value.ToString());
+        }
+
+        public UrlBuilder AddParameter(string name, bool value)
+        {
+            return AddParameter(name, value.ToString());
+        }
+        /// <summary>
+        /// Builds the final URL string.
+        /// </summary>
+        public string Build()
+        {
+            if (dictionary.Count == 0)
+            {
+                return url;
+            }
+
+            string parametersString = string.Join("&", dictionary.Select(p => $"{p.Key}={p.Value}"));
+
+            return $"{url}?{parametersString}";
+        }
+
+        public override string ToString() => Build();
+
+        public bool TryGetValue<T>(string parameterName, out T? value)
+        {
+            value = default;
+
+            if(parameterName is null)
+            {
+                return false;
+            }
+
+            if(!dictionary.TryGetValue(parameterName, out string? @string))
+            {
+                return false;
+            }
+
+            @string = WebUtility.UrlDecode(@string);
+
+            if(!Core.Query.TryConvert(@string, out value))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ DiGi.WebAPI/Create/UrlBuilder.cs
@@ -0,0 +1,42 @@
+﻿using DiGi.WebAPI.Classes;
+using System.Net;
+
+namespace DiGi.WebAPI
+{
+    public static partial class Create
+    {
+        public static UrlBuilder? UrlBuilder(this string url)
+        {
+            if(string.IsNullOrWhiteSpace(url))
+            {
+                return null;
+            }
+
+            string[] parts = url.Split('?');
+            
+            string baseUrl = parts[0];
+            
+            UrlBuilder builder = new (baseUrl);
+
+            if (parts.Length > 1)
+            {
+                string query = parts[1];
+                string[] pairs = query.Split('&');
+
+                foreach (string pair in pairs)
+                {
+                    string[] kvp = pair.Split('=');
+                    if (kvp.Length == 2)
+                    {
+                        string key = kvp[0];
+                        // Decodes %2F to /, %23 to #, etc.
+                        string value = WebUtility.UrlDecode(kvp[1]);
+                        builder.AddParameter(key, value);
+                    }
+                }
+            }
+
+            return builder;
+        }
+    }
+}
