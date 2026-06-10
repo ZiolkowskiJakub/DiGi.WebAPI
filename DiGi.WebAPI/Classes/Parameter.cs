d698dc25d67c5b1821c11789a9834b2bdc20c94f
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2026-06-10 22:28:06 +0200

    Parameter abstract class implemented

---
+++ DiGi.WebAPI/Classes/Parameter.cs
@@ -0,0 +1,29 @@
+﻿using DiGi.Core.Classes;
+using System.Text.Json.Nodes;
+
+namespace DiGi.WebAPI.Classes
+{
+    /// <summary>
+    /// Abstract class for input parameters for endpoint.
+    /// </summary>
+    public abstract class Parameter : SerializableObject
+    {
+        public Parameter(Parameter parameter)
+            : base(parameter)
+        {
+
+        }
+
+        public Parameter(JsonObject jsonObject)
+            : base(jsonObject)
+        {
+
+        }
+
+        public Parameter()
+            : base()
+        {
+
+        }
+    }
+}
