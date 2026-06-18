63132113fd8fbfc7f5e2c2fb679ed8bd463ae46a
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2026-06-18 20:28:50 +0200

    Documentation and README.md updated

--- DiGi.WebAPI/Classes/Parameter.cs
+++ DiGi.WebAPI/Classes/Parameter.cs
@@ -8,18 +8,29 @@ namespace DiGi.WebAPI.Classes
     /// </summary>
     public abstract class Parameter : SerializableObject
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Parameter"/> class by copying another instance of the same type.
+        /// </summary>
+        /// <param name="parameter">The <see cref="Parameter"/> instance to copy from.</param>
         public Parameter(Parameter parameter)
             : base(parameter)
         {
 
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Parameter"/> class using the specified JSON object.
+        /// </summary>
+        /// <param name="jsonObject">The <see cref="JsonObject"/> that contains the initialization data.</param>
         public Parameter(JsonObject jsonObject)
             : base(jsonObject)
         {
 
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Parameter"/> class.
+        /// </summary>
         public Parameter()
             : base()
         {
