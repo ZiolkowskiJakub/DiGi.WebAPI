04227b89c954e8477bf45025baa5101d3dffd5fd
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2026-09-21 18:57:05 +0200

    Fix #38: add image/jpeg to WebAPIController [Produces] list
    The deployed host's content-negotiation filter reads the class-level
    [Produces] to build its allowed content-type whitelist. image/jpeg was
    missing, causing the imagebyreference endpoint to answer 406 for every
    Accept header variant. Adding it to the list is the minimal fix.
    
    Refs: ZiolkowskiJakub/DiGi.GIS.WebAPI#38

--- DiGi.WebAPI/Classes/WebAPIController.cs
+++ DiGi.WebAPI/Classes/WebAPIController.cs
@@ -7,7 +7,7 @@ namespace DiGi.WebAPI.Classes
     /// Serves as the base class for all WebAPI controllers, providing common MVC and API functionality.
     /// </summary>
     [ApiExplorerSettings(IgnoreApi = true)]
-    [Produces("application/json")]
+    [Produces("application/json", "image/jpeg")]
     [Consumes("application/json")]
     public abstract class WebAPIController : ControllerBase, IWebAPIController
     {
